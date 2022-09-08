68d7238af68746f90dbd3801e705f87a59745464
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2022-09-08 11:23:50 +0200

    [StorageList] fix CartContext found Order as cart

---
+++ Repository/StorageListRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.org)
+ * @license    https://www.coreshop.org/license     GPLv3 and CCL
+ */
+
+declare(strict_types=1);
+
+namespace CoreShop\Component\StorageList\Repository;
+
+use CoreShop\Component\Resource\Repository\RepositoryInterface;
+use CoreShop\Component\StorageList\Model\StorageListInterface;
+
+interface StorageListRepositoryInterface extends RepositoryInterface
+{
+    public function findByStorageListId(int $id): ?StorageListInterface;
+}
